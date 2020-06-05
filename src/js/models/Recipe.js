b6f123195edcf72c4be1f72f82f1dc26b9cc0c39
herbjrob <43552696+herbjrob@users.noreply.github.com>
2020-06-05 15:00:57 -0400

    Completed all Model Modules

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -31,6 +31,7 @@ export default class Recipe {
 	parseIngredients() {
 		const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoon', 'teaspoons', 'cups', 'pounds'];
 		const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+		const units = [...unitsShort, 'kg', 'g'];
 
 		const newIngredients = this.ingredients.map(el => {
 			// Uniform units
@@ -44,7 +45,7 @@ export default class Recipe {
 		
 			// Parse ingredients into count, unit and ingredient
 			const arrIng = ingredient.split(' ');
-			const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+			const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 			
 			let objIng;
 			if (unitIndex > -1) {
@@ -82,5 +83,15 @@ export default class Recipe {
 		});
 
 		this.ingredients = newIngredients;
+	}
+
+	updateServings (type) {
+		const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
+
+		this.ingredients.forEach(ing => {
+			ing.count *= (newServings/this.servings);
+		})
+
+		this.servings = newServings;
 	}
 }
